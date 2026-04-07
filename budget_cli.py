84461faa84b461dc340f7f5e6c204fbe12b9d7fc
PrincetonAfeez <princetonafeez@gmail.com>
2026-04-07 16:23:36 -0700

    importing modules/packages

---
+++ budget_cli.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from typing import cast
+
+import budget
+from schemas import BudgetAllocation, BudgetCategoryProfile, BudgetComparisonResult
+from storage import format_money
+
+
